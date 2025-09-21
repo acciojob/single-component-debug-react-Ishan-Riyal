763c132b34c3ab9dc379b1e7cad4f94037ee376f
Ishan Riyal <ishanriyal07@gmail.com>
2025-09-21 16:35:34 +0530

    Completed ReactDOM.render assignment

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-
-
+const rootNode = document.getElementById("root");
+ReactDOM.render(<App />, rootNode);
